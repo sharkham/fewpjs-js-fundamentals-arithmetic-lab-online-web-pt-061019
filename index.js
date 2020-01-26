a18502715eef071e724eb6c3bd7870b6f0577556
Sam Markham <sam.e.markham@gmail.com>
2020-01-26 13:19:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 24 % 5;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
